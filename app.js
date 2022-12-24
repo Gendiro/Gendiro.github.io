cb27a5504a598acad7657a9085ef452f14be0c16
Gendiro <grabberisonfire@gmail.com>
2022-12-24 20:31:35 +0300

    plan version 2

--- app.js
+++ app.js
@@ -15,4 +15,16 @@ hiddenElements.forEach((el) => {observer.observe(el)});
 function change_pic(index) {
     const pic = document.getElementById("plan_img");
     pic.src = "floor_" + index + ".png";
+    const description = document.getElementById("description");
+    switch(index) {
+        case 1:
+            description.innerHTML = "Этот этаж будет служить входом в великий шпиль, ознаменовывая путь вверх или вниз по великому шпилю, ну и все такое, викинг, делай блять описание ну ебанный рот, я устал кодить";
+            break;
+        case 2:
+            description.innerHTML = "Описание для второго этажа чтоб отличить можно было";
+            break;
+        default:
+            description.innerHTML = "Пшел отсюдова! Не написано еще!";
+            break;
+    }
 }
