8117d0207028a725354113225e65735a557a4f70
Roman <grabberisonfire@gmail.com>
2024-04-16 01:16:49 +0300

    version 0.2.0: new design
    version 0.2.0.1 formatted
    
    version 0.2.0.2 ts checks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,58 @@
-<script lang="ts">
-	import Projects from './projects.svelte';
-	import GeneralInfo from './general_info.svelte';
-	import Stack from './stack.svelte';
+<script>
+	import FadeInText from './FadeInText.svelte';
 </script>
 
-<GeneralInfo />
+<div id="main-page">
+	<nav>
+		<p>Portfolio</p>
+		<p>Contact me</p>
+	</nav>
+	<main>
+		<h1><FadeInText text="> Hello, I'm Roman Pogrebnyak" /></h1>
+		<div id="short-bio">
+			<p><FadeInText text="> Cybersecurity student" /></p>
+			<p><FadeInText text="> Fullstack webdev enthusiast" /></p>
+			<p><FadeInText text="> Love to code in general" /></p>
+		</div>
+		<img src="images/cat.png" alt="cute pfp" id="cat-pfp" />
+	</main>
+</div>
 
-<Stack />
-
-<Projects />
+<style>
+	:root {
+		background: url('images/main_background.png') #1b1b1b no-repeat bottom left / 100% auto;
+		width: 100vw;
+		height: 100vh;
+	}
+	#main-page {
+		color: white;
+		font-family: 'Roboto Light', serif;
+		font-weight: bold;
+	}
+	#main-page nav {
+		display: inline-flex;
+		font-size: xxx-large;
+		gap: 100px;
+		position: absolute;
+		right: 100px;
+	}
+	#main-page h1 {
+		font-size: 80px;
+		position: absolute;
+		left: 25%;
+		top: 17%;
+	}
+	#main-page #short-bio {
+		width: 35%;
+		font-size: 50px;
+		position: absolute;
+		right: 5%;
+		top: 40%;
+		line-height: 40px;
+	}
+	#cat-pfp {
+		position: absolute;
+		bottom: 3vh;
+		left: 7%;
+	}
+</style>
---
+++ src/routes/FadeInText.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let cur_text = '';
+	export let text: string;
+	export let delay = 100;
+
+	onMount(() => {
+		let index = 0;
+		const interval = setInterval(() => {
+			cur_text += text[index];
+			index++;
+			if (index === text.length) {
+				clearInterval(interval);
+			}
+		}, delay);
+
+		return () => clearInterval(interval);
+	});
+</script>
+
+{cur_text}
