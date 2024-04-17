cc4d72aaf34b88822c858806d434d8ef8e1be50a
Roman <grabberisonfire@gmail.com>
2024-04-17 18:24:02 +0300

    version 0.2.3 new pages created, some animations added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,54 @@
-<script>
+<script lang="ts">
 	import FadeInText from './FadeInText.svelte';
 	import cat_png from '$lib/images/cat.png';
+	import {fly} from 'svelte/transition';
+	import { onDestroy, onMount } from 'svelte';
+	import { goto } from "$app/navigation";
+
+	let isLoading = true;
+	onMount(() => {
+			isLoading = false;
+	});
+
+	onDestroy(() => {
+			isLoading = true;
+	});
+
+	const goto_fade = (addr: string) => {
+		goto(addr);
+		isLoading = true;
+	};
 </script>
 
-<div id="main-page">
-	<nav>
-		<p>Portfolio</p>
-		<p>Contact me</p>
-	</nav>
-	<main>
-		<h1><FadeInText text="> Hello, I'm Roman Pogrebnyak" /></h1>
-		<div id="short-bio">
-			<p><FadeInText text="> Cybersecurity student" /></p>
-			<p><FadeInText text="> Fullstack webdev enthusiast" /></p>
-			<p><FadeInText text="> Love to code in general" /></p>
-		</div>
-		<img src={cat_png} alt="cute pfp" id="cat-pfp" />
-	</main>
-</div>
+{#if !isLoading}
+	<div id="main-page" out:fly={{y:500, duration:1000}}>
+		<nav>
+			<button on:click={() => goto_fade("/portfolio")} class="main-button">Portfolio</button>
+			<button on:click={() => goto_fade("/contacts")} class="main-button">Contact me</button>
+		</nav>
+		<main>
+			<h1><FadeInText text="> Hello, I'm Roman Pogrebnyak" /></h1>
+			<div id="short-bio">
+				<p><FadeInText text="> Cybersecurity student" /></p>
+				<p><FadeInText text="> Fullstack webdev enthusiast" /></p>
+				<p><FadeInText text="> Love to code in general" /></p>
+			</div>
+			<img src={cat_png} alt="cute pfp" id="cat-pfp" />
+		</main>
+	</div>
+{:else}
+	Your website is loading...
+{/if}
 
 <style>
 	:root {
-		background: url($lib/images/main_background.png) #1b1b1b no-repeat bottom left / 100% auto;
+			background-color: #1b1b1b;
+	}
+	#main-page {
+		background: url($lib/images/main_background.svg) #1b1b1b no-repeat bottom left / 100% auto;
 		width: 100vw;
 		height: 100vh;
+		display: flex;
 	}
 	#main-page {
 		color: white;
@@ -36,6 +61,7 @@
 		gap: 100px;
 		position: absolute;
 		right: 100px;
+		top: 50px;
 	}
 	#main-page h1 {
 		font-size: 80px;
@@ -56,4 +82,14 @@
 		bottom: 3vh;
 		left: 7%;
 	}
+  .main-button {
+      background-color: transparent;
+      border-width: 0;
+      font-family: inherit;
+      font-size: inherit;
+      font-style: inherit;
+      font-weight: inherit;
+      line-height: inherit;
+			color: white;
+  }
 </style>
---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<div id="contact-page">
+
+</div>
+
+<style>
+#contact-page {
+		background-color: #1b1b1b;
+		width: 100vw;
+		height: 100vh;
+}
+</style>
